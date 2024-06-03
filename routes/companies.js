88298bdd6dd3805b0845c1182a6f136c1da7b0c5
Sajid Jaber <sajidjaber@yahoo.com>
2024-06-03 11:59:13 -0500

    added search by code

--- routes/companies.js
+++ routes/companies.js
@@ -3,13 +3,23 @@ const router = new express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
 
-/** Get companies */
+/** Get companies all*/
 
-router.get("/", async function  (req, res, next) {
+router.get("/", async function (req, res, next) {
   const results = await db.query(`
     SELECT * FROM companies`);
 
-  return res.json(results.rows);
+  return res.json({ companies: results.rows });
 });
+
+
+router.get("/:name", async function (req, res, next) {
+
+  const name = req.params.name
+  const results = await db.query(`
+    SELECT * FROM companies WHERE code = '${name}'`);
+  return res.json({ company: results.rows });
+});
+
 
 module.exports = router;
