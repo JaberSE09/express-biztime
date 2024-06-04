91f8353b900291067d0ad3a3c7bc51e3c7f13677
Sajid Jaber <sajidjaber@yahoo.com>
2024-06-04 09:09:36 -0500

    companies

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
+const slugify = require("slugify");
 
 /** Get companies all*/
 
@@ -21,5 +22,22 @@ router.get("/:name", async function (req, res, next) {
   return res.json({ company: results.rows });
 });
 
+router.post("/", async function (req, res, next) {
+  try {
+    let {name, description} = req.body;
+    let code = slugify(name, {lower: true});
 
+    const result = await db.query(
+          `INSERT INTO companies (code, name, description) 
+           VALUES ($1, $2, $3) 
+           RETURNING code, name, description`,
+        [code, name, description]);
+
+    return res.status(201).json({"company": result.rows[0]});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
 module.exports = router;
